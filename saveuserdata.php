3b0422d28a14db8cbeb43deb73f056708f1d9aa3
Bill Ferster <bferster@virginia.edu>
2014-05-27 17:01:44 -0400

    assess php/sql

--- saveuserdata.php
+++ saveuserdata.php
@@ -8,26 +8,31 @@ header('Access-Control-Max-Age: 1000');
 require_once('config.php');
 			
 	$password="";												
+	$show="";											
 	$email="";												
-	$event="";												
+	$event="";	
 	
 	$email=$_REQUEST['email'];									// Get email
 	$password=$_REQUEST['password'];							// Get password
+	$show=$_REQUEST['show'];									// Get shownum
+	
 	if (isSet($_REQUEST['event'])) 								// If set
 		$event=$_REQUEST['event'];								// Get it
 										
-	$query="SELECT * FROM qusers WHERE email = '".$email."'"; 	// Look existing one	
+	$query="SELECT * FROM qusers WHERE email = '".$email."' AND showNum = '".$show."'";	// Look for existing one
+	
 	$result=mysql_query($query);								// Query
 	if ($result == false) {										// Bad query
-		print("-1");											// Show error 
+		print(-1);												// Show error 
 		mysql_close();											// Close session
 		exit();													// Quit
 		}
 	if (!mysql_numrows($result)) {								// If not found, add it
-		$query="INSERT INTO qusers (email, password, events ) VALUES ('";
+		$query="INSERT INTO qusers (email, password, showNum, events ) VALUES ('";
 		$query.=addEscapes($email)."','";						// Email
 		$query.=addEscapes($password)."','";					// Password
-		$query.=addEscapes("Created new user account\n")."'";	// Event
+		$query.=addEscapes($show)."','";						// Password
+		$query.=addEscapes("Created new user account\n")."')";	// Event
 		$result=mysql_query($query);							// Add row
 		if ($result == false)									// Bad save
 			print("-2");										// Show error 
@@ -37,13 +42,15 @@ require_once('config.php');
 	else{														// We have one already
 		$oldpass=mysql_result($result,0,"password");			// Get old password		
 		if ($oldpass && ($password != $oldpass)) {				// Passwords don't match
-			print("pass");										// Show error 
+			print($show."----".mysql_result($result,0,"show"));										// Show error 
 			mysql_close();										// Close session
 			exit();												// Quit
 			}
 		$id=mysql_result($result,0,"id");						// Get id
-		if ($id != "") 											// If valid
-			$query="UPDATE qusers SET events = CONCAT(events, $event) WHERE id = '".$id."'";
+		if ($id != "") {										// If valid
+			$query="UPDATE qusers SET events = CONCAT(events, '$event') WHERE id = '".$id."'";
+			$result=mysql_query($query);						// Add to field
+			}
 		if ($result == false)									// Bad update
 			print("-4");										// Show error 
 		else
