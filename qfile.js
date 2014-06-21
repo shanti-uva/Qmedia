94930a2340156232116a233cfd2e60d0e59aecc6
Bill Ferster <bferster@virginia.edu>
2014-06-21 16:37:46 -0400

    Flickr shell

--- qfile.js
+++ qfile.js
@@ -323,7 +323,6 @@
 		$("#lightBoxTitle").html("<span style='color:#990000'>"+msg+"</span>");	// Put new
 	}
 	
-	
 	function MakeColorDot(title, name, color)								// MAKE COLORPICKER DOT
 	{
 			var str=title+"&nbsp;&nbsp;<div id='"+name+"' "; 
@@ -460,6 +459,41 @@
 		$(".ui-dialog").css({"border-radius":"14px", "box-shadow":"4px 4px 8px #ccc"});
  		$(".ui-button").css({"border-radius":"30px","outline":"none"});
  	}
+
+
+	function GetFlickrImage(callback)										// GET FLICKR IMAGE
+	{
+		$("#alertBoxDiv").remove();												// Remove any old ones
+		$("body").append("<div class='unselectable' id='alertBoxDiv'></div>");														
+		var str="<p><img src='images/qlogo32.png' style='vertical-align:-10px'/>&nbsp;&nbsp;";								
+		str+="<span style='font-size:18px;text-shadow:1px 1px #ccc;color:#000099'><b>Get Flickr Image</b></span><p>";
+		str+="<div style='font-size:14px;margin:14px'>";
+		str+="<br><br><div style='display:inline-block;width:300px;max-height:200px;overflow-y:auto;background-color:#f8f8f8;padding:8px;border:1px solid #999;border-radius:8px'>";		// Scrollable container
+		str+="<table id='collectTable' style='font-size:13px;width:100%;padding:0px;border-collapse:collapse;'>";	// Add table
+		str+="<tr><td><b>Collection</b></td><td width='20'></td></tr>";			// Add header
+		str+="<tr><td colspan='2'><hr></td></tr>";								// Add rule
+		str+="</table></div>&nbsp;&nbsp;&nbsp;"									// End table
+	
+		str+="<div style='vertical-align:top;display:inline-block;width:300px;max-height:200px;overflow-y:auto;background-color:#f8f8f8;padding:8px;border:1px solid #999;border-radius:8px'>";		// Scrollable container
+		str+="<dl id='setTable' style='font-size:13px;margin-top:2px;margin-bottom:2px'>";		// Add table
+		str+="<dt><b>Set</b></dt>";												// Add header
+		str+="<dt><hr></dt>";													// Add rule
+		str+="</dl></div><div style='font-size:12px'<br><p><hr></p>";			// End table
+	
+		$("#alertBoxDiv").append(str+"</div>");	
+		$("#alertBoxDiv").dialog({ width:800, buttons: {
+					            	"Done":  function() { $(this).remove(); }
+									}});	
+		$(".ui-dialog-titlebar").hide();
+		$(".ui-dialog-buttonpane.ui-widget-content.ui-helper-clearfix").css("border","none");
+		$(".ui-dialog").css({"border-radius":"14px", "box-shadow":"4px 4px 8px #ccc"});
+ 		$(".ui-button").css({"border-radius":"30px","outline":"none"});
+ 	}
+
+
+
+
+
 
 	function AddEscapes(str)												// ESCAPE TEXT STRING
 	{
